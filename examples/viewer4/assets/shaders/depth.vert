d57210fbe0841a9012bfc47018f1ccba1fa67323
o-eduardo <ca,eduardo94@gmail.com>
2021-04-23 03:52:53 -0300

    atividade 3 - esboco tour de planetas com camera ajustada, planeta marte e sem widgets

--- examples/viewer4/assets/shaders/depth.vert
+++ examples/viewer4/assets/shaders/depth.vert
@@ -1,7 +1,7 @@
 #version 410
 
 layout(location = 0) in vec3 inPosition;
-
+uniform vec4 color;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projMatrix;
@@ -11,8 +11,8 @@ out vec4 fragColor;
 void main() {
   vec4 posEyeSpace = viewMatrix * modelMatrix * vec4(inPosition, 1);
 
-  float i = 1.0 - (-posEyeSpace.z / 3.0);
-  fragColor = vec4(i, i, i, 1);
+  float i = 1.0 - (-posEyeSpace.z / 2.0);
+  fragColor = vec4(i, i, i, 1) * color;
 
   gl_Position = projMatrix * posEyeSpace;
 }
--- examples/viewer4/openglwindow.cpp
+++ examples/viewer4/openglwindow.cpp
@@ -10,7 +10,7 @@
 void OpenGLWindow::handleEvent(SDL_Event& event) {
   glm::ivec2 mousePosition;
   SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
-
+/*
   if (event.type == SDL_MOUSEMOTION) {
     m_trackBallModel.mouseMove(mousePosition);
     m_trackBallLight.mouseMove(mousePosition);
@@ -35,7 +35,7 @@ void OpenGLWindow::handleEvent(SDL_Event& event) {
     m_zoom += (event.wheel.y > 0 ? 1.0f : -1.0f) / 5.0f;
     m_zoom = glm::clamp(m_zoom, -1.5f, 1.0f);
   }
-
+*/
 //Cameras adicionais
 
 if (event.type == SDL_KEYDOWN) {
@@ -47,8 +47,6 @@ if (event.type == SDL_KEYDOWN) {
       m_panSpeed = -1.0f;
     if (event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d)
       m_panSpeed = 1.0f;
-    if (event.key.keysym.sym == SDLK_q) m_truckSpeed = -1.0f;
-    if (event.key.keysym.sym == SDLK_e) m_truckSpeed = 1.0f;
   }
   if (event.type == SDL_KEYUP) {
     if ((event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w) &&
@@ -63,8 +61,6 @@ if (event.type == SDL_KEYDOWN) {
     if ((event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d) &&
         m_panSpeed > 0)
       m_panSpeed = 0.0f;
-    if (event.key.keysym.sym == SDLK_q && m_truckSpeed < 0) m_truckSpeed = 0.0f;
-    if (event.key.keysym.sym == SDLK_e && m_truckSpeed > 0) m_truckSpeed = 0.0f;
   }
 
 
@@ -82,7 +78,7 @@ void OpenGLWindow::initializeGL() {
   }
 
   // Load default model
-  loadModel(getAssetsPath() + "roman_lamp.obj");
+  loadModel(getAssetsPath() + "Mars 2K.obj");
   m_mappingMode = 3;  // "From mesh" option
 
   // Initial trackball spin
@@ -91,7 +87,7 @@ void OpenGLWindow::initializeGL() {
 }
 
 void OpenGLWindow::loadModel(std::string_view path) {
-  m_model.loadDiffuseTexture(getAssetsPath() + "maps/pattern.png");
+  m_model.loadDiffuseTexture(getAssetsPath() + "maps/Diffuse_2K.png");
   m_model.loadFromFile(path);
   m_model.setupVAO(m_programs.at(m_currentProgramIndex));
   m_trianglesToDraw = m_model.getNumTriangles();
@@ -102,6 +98,7 @@ void OpenGLWindow::loadModel(std::string_view path) {
   m_Ks = m_model.getKs();
   m_shininess = m_model.getShininess();
 }
+
 
 void OpenGLWindow::paintGL() {
   update();
@@ -172,232 +169,29 @@ void OpenGLWindow::paintGL() {
 void OpenGLWindow::paintUI() {
   abcg::OpenGLWindow::paintUI();
 
-  // File browser for models
-  static ImGui::FileBrowser fileDialogModel;
-  fileDialogModel.SetTitle("Load 3D Model");
-  fileDialogModel.SetTypeFilters({".obj"});
-  fileDialogModel.SetWindowSize(m_viewportWidth * 0.8f,
-                                m_viewportHeight * 0.8f);
 
-  // File browser for textures
-  static ImGui::FileBrowser fileDialogTex;
-  fileDialogTex.SetTitle("Load Texture");
-  fileDialogTex.SetTypeFilters({".jpg", ".png"});
-  fileDialogTex.SetWindowSize(m_viewportWidth * 0.8f, m_viewportHeight * 0.8f);
+  // Only in WebGL
+  #if defined(__EMSCRIPTEN__)
+    fileDialogModel.SetPwd(getAssetsPath());
+    fileDialogTex.SetPwd(getAssetsPath() + "/maps");
+  #endif
+   // Slider will be stretched horizontally
+  m_trianglesToDraw = m_model.getNumTriangles();
 
-// Only in WebGL
-#if defined(__EMSCRIPTEN__)
-  fileDialogModel.SetPwd(getAssetsPath());
-  fileDialogTex.SetPwd(getAssetsPath() + "/maps");
-#endif
+  glEnable(GL_CULL_FACE);
 
-  // Create main window widget
-  {
-    auto widgetSize{ImVec2(222, 190)};
+  // CW/CCW combo box
+  glFrontFace(GL_CCW);
 
-    if (!m_model.isUVMapped()) {
-      // Add extra space for static text
-      widgetSize.y += 26;
-    }
+  // Projection combo box
+  auto aspect{static_cast<float>(m_viewportWidth) /
+                static_cast<float>(m_viewportHeight)};
+  m_projMatrix = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 5.0f);
+  // UV mapping box
 
-    ImGui::SetNextWindowPos(ImVec2(m_viewportWidth - widgetSize.x - 5, 5));
-    ImGui::SetNextWindowSize(widgetSize);
-    auto flags{ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDecoration};
-    ImGui::Begin("Widget window", nullptr, flags);
-
-    // Menu
-    {
-      bool loadModel{};
-      bool loadDiffTex{};
-      if (ImGui::BeginMenuBar()) {
-        if (ImGui::BeginMenu("File")) {
-          ImGui::MenuItem("Load 3D Model...", nullptr, &loadModel);
-          ImGui::MenuItem("Load Diffuse Texture...", nullptr, &loadDiffTex);
-          ImGui::EndMenu();
-        }
-        ImGui::EndMenuBar();
-      }
-      if (loadModel) fileDialogModel.Open();
-      if (loadDiffTex) fileDialogTex.Open();
-    }
-
-    // Slider will be stretched horizontally
-    ImGui::PushItemWidth(widgetSize.x - 16);
-    ImGui::SliderInt("", &m_trianglesToDraw, 0, m_model.getNumTriangles(),
-                     "%d triangles");
-    ImGui::PopItemWidth();
-
-    static bool faceCulling{};
-    ImGui::Checkbox("Back-face culling", &faceCulling);
-
-    if (faceCulling) {
-      glEnable(GL_CULL_FACE);
-    } else {
-      glDisable(GL_CULL_FACE);
-    }
-
-    // CW/CCW combo box
-    {
-      static std::size_t currentIndex{};
-      std::vector<std::string> comboItems{"CCW", "CW"};
-
-      ImGui::PushItemWidth(120);
-      if (ImGui::BeginCombo("Front face",
-                            comboItems.at(currentIndex).c_str())) {
-        for (auto index : iter::range(comboItems.size())) {
-          const bool isSelected{currentIndex == index};
-          if (ImGui::Selectable(comboItems.at(index).c_str(), isSelected))
-            currentIndex = index;
-          if (isSelected) ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-      }
-      ImGui::PopItemWidth();
-
-      if (currentIndex == 0) {
-        glFrontFace(GL_CCW);
-      } else {
-        glFrontFace(GL_CW);
-      }
-    }
-
-    // Projection combo box
-    {
-      static std::size_t currentIndex{};
-      std::vector<std::string> comboItems{"Perspective", "Orthographic"};
-
-      ImGui::PushItemWidth(120);
-      if (ImGui::BeginCombo("Projection",
-                            comboItems.at(currentIndex).c_str())) {
-        for (auto index : iter::range(comboItems.size())) {
-          const bool isSelected{currentIndex == index};
-          if (ImGui::Selectable(comboItems.at(index).c_str(), isSelected))
-            currentIndex = index;
-          if (isSelected) ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-      }
-      ImGui::PopItemWidth();
-
-      auto aspect{static_cast<float>(m_viewportWidth) /
-                  static_cast<float>(m_viewportHeight)};
-      if (currentIndex == 0) {
-        m_projMatrix =
-            glm::perspective(glm::radians(45.0f), aspect, 0.1f, 5.0f);
-
-      } else {
-        m_projMatrix =
-            glm::ortho(-1.0f * aspect, 1.0f * aspect, -1.0f, 1.0f, 0.1f, 5.0f);
-      }
-    }
-
-    // Shader combo box
-    {
-      static std::size_t currentIndex{};
-
-      ImGui::PushItemWidth(120);
-      if (ImGui::BeginCombo("Shader", m_shaderNames.at(currentIndex))) {
-        for (auto index : iter::range(m_shaderNames.size())) {
-          const bool isSelected{currentIndex == index};
-          if (ImGui::Selectable(m_shaderNames.at(index), isSelected))
-            currentIndex = index;
-          if (isSelected) ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-      }
-      ImGui::PopItemWidth();
-
-      // Set up VAO if shader program has changed
-      if (static_cast<int>(currentIndex) != m_currentProgramIndex) {
-        m_currentProgramIndex = currentIndex;
-        m_model.setupVAO(m_programs.at(m_currentProgramIndex));
-      }
-    }
-
-    if (!m_model.isUVMapped()) {
-      ImGui::TextColored(ImVec4(1, 1, 0, 1), "Mesh has no UV coords.");
-    }
-
-    // UV mapping box
-    {
-      std::vector<std::string> comboItems{"Triplanar", "Cylindrical",
-                                          "Spherical"};
-
-      if (m_model.isUVMapped()) comboItems.emplace_back("From mesh");
-
-      ImGui::PushItemWidth(120);
-      if (ImGui::BeginCombo("UV mapping",
-                            comboItems.at(m_mappingMode).c_str())) {
-        for (auto index : iter::range(comboItems.size())) {
-          const bool isSelected{m_mappingMode == static_cast<int>(index)};
-          if (ImGui::Selectable(comboItems.at(index).c_str(), isSelected))
-            m_mappingMode = index;
-          if (isSelected) ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-      }
-      ImGui::PopItemWidth();
-    }
-
-    ImGui::End();
-  }
-
-  // Create window for light sources
-  if (m_currentProgramIndex < 4) {
-    auto widgetSize{ImVec2(222, 244)};
-    ImGui::SetNextWindowPos(ImVec2(m_viewportWidth - widgetSize.x - 5,
-                                   m_viewportHeight - widgetSize.y - 5));
-    ImGui::SetNextWindowSize(widgetSize);
-    ImGui::Begin(" ", nullptr, ImGuiWindowFlags_NoDecoration);
-
-    ImGui::Text("Light properties");
-
-    // Slider to control light properties
-    ImGui::PushItemWidth(widgetSize.x - 36);
-    ImGui::ColorEdit3("Ia", &m_Ia.x, ImGuiColorEditFlags_Float);
-    ImGui::ColorEdit3("Id", &m_Id.x, ImGuiColorEditFlags_Float);
-    ImGui::ColorEdit3("Is", &m_Is.x, ImGuiColorEditFlags_Float);
-    ImGui::PopItemWidth();
-
-    ImGui::Spacing();
-
-    ImGui::Text("Material properties");
-
-    // Slider to control material properties
-    ImGui::PushItemWidth(widgetSize.x - 36);
-    ImGui::ColorEdit3("Ka", &m_Ka.x, ImGuiColorEditFlags_Float);
-    ImGui::ColorEdit3("Kd", &m_Kd.x, ImGuiColorEditFlags_Float);
-    ImGui::ColorEdit3("Ks", &m_Ks.x, ImGuiColorEditFlags_Float);
-    ImGui::PopItemWidth();
-
-    // Slider to control the specular shininess
-    ImGui::PushItemWidth(widgetSize.x - 16);
-    ImGui::SliderFloat("", &m_shininess, 0.0f, 500.0f, "shininess: %.1f");
-    ImGui::PopItemWidth();
-
-    ImGui::End();
-  }
-
-  fileDialogModel.Display();
-  if (fileDialogModel.HasSelected()) {
-    loadModel(fileDialogModel.GetSelected().string());
-    fileDialogModel.ClearSelected();
-
-    if (m_model.isUVMapped()) {
-      // Use mesh texture coordinates if available...
-      m_mappingMode = 3;
-    } else {
-      // ...or triplanar mapping otherwise
-      m_mappingMode = 0;
-    }
-  }
-
-  fileDialogTex.Display();
-  if (fileDialogTex.HasSelected()) {
-    m_model.loadDiffuseTexture(fileDialogTex.GetSelected().string());
-    fileDialogTex.ClearSelected();
-  }
+  m_mappingMode = 3;
 }
+
 
 void OpenGLWindow::resizeGL(int width, int height) {
   m_viewportWidth = width;
