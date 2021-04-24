aaa52d35cc4eac4a04df19eddbe14ad91ff3faa4
o-eduardo <ca,eduardo94@gmail.com>
2021-04-24 19:33:15 -0300

    Inclui pasta com atividade 3 - fazendo - PlanetTour

---
+++ examples/planettour/openglwindow.cpp
@@ -0,0 +1,206 @@
+#include "openglwindow.hpp"
+
+#include <imgui.h>
+
+#include <cppitertools/itertools.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+#include "imfilebrowser.h"
+#include "model.hpp"
+
+
+void OpenGLWindow::handleEvent(SDL_Event& event) {
+  glm::ivec2 mousePosition;
+  SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
+
+//Cameras adicionais
+
+if (event.type == SDL_KEYDOWN) {
+    if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w)
+      m_dollySpeed = 1.0f;
+    if (event.key.keysym.sym == SDLK_DOWN || event.key.keysym.sym == SDLK_s)
+      m_dollySpeed = -1.0f;
+    if (event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_a)
+      m_panSpeed = -1.0f;
+    if (event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d)
+      m_panSpeed = 1.0f;
+  }
+  if (event.type == SDL_KEYUP) {
+    if ((event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w) &&
+        m_dollySpeed > 0)
+      m_dollySpeed = 0.0f;
+    if ((event.key.keysym.sym == SDLK_DOWN || event.key.keysym.sym == SDLK_s) &&
+        m_dollySpeed < 0)
+      m_dollySpeed = 0.0f;
+    if ((event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_a) &&
+        m_panSpeed < 0)
+      m_panSpeed = 0.0f;
+    if ((event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d) &&
+        m_panSpeed > 0)
+      m_panSpeed = 0.0f;
+  }
+
+
+}
+
+void OpenGLWindow::initializeGL() {
+  glClearColor(0, 0, 0, 1);
+  glEnable(GL_DEPTH_TEST);
+
+  // Create programs
+  for (const auto& name : m_shaderNames) {
+    auto path{getAssetsPath() + "shaders/" + name};
+    auto program{createProgramFromFile(path + ".vert", path + ".frag")};
+    m_programs.push_back(program);
+  }
+
+  // Load default model
+  loadModel(getAssetsPath() + "Mars 2K.obj");
+  m_mappingMode = 3;  // "From mesh" option
+
+  // Initial trackball spin
+  m_trackBallModel.setAxis(glm::normalize(glm::vec3(1, 1, 1)));
+  m_trackBallModel.setVelocity(0.0001f);
+}
+
+void OpenGLWindow::loadModel(std::string_view path) {
+  m_model.loadDiffuseTexture(getAssetsPath() + "maps/Diffuse_2K.png");
+  m_model.loadFromFile(path);
+  m_model.setupVAO(m_programs.at(m_currentProgramIndex));
+  m_trianglesToDraw = m_model.getNumTriangles();
+
+  // Use material properties from the loaded model
+  m_Ka = m_model.getKa();
+  m_Kd = m_model.getKd();
+  m_Ks = m_model.getKs();
+  m_shininess = m_model.getShininess();
+}
+
+
+void OpenGLWindow::paintGL() {
+  update();
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glViewport(0, 0, m_viewportWidth, m_viewportHeight);
+
+  // Use currently selected program
+  const auto program{m_programs.at(m_currentProgramIndex)};
+  glUseProgram(program);
+
+  // Get location of uniform variables
+  GLint viewMatrixLoc{glGetUniformLocation(program, "viewMatrix")};
+  GLint projMatrixLoc{glGetUniformLocation(program, "projMatrix")};
+  GLint modelMatrixLoc{glGetUniformLocation(program, "modelMatrix")};
+  GLint normalMatrixLoc{glGetUniformLocation(program, "normalMatrix")};
+  GLint lightDirLoc{glGetUniformLocation(program, "lightDirWorldSpace")};
+  GLint shininessLoc{glGetUniformLocation(program, "shininess")};
+  GLint IaLoc{glGetUniformLocation(program, "Ia")};
+  GLint IdLoc{glGetUniformLocation(program, "Id")};
+  GLint IsLoc{glGetUniformLocation(program, "Is")};
+  GLint KaLoc{glGetUniformLocation(program, "Ka")};
+  GLint KdLoc{glGetUniformLocation(program, "Kd")};
+  GLint KsLoc{glGetUniformLocation(program, "Ks")};
+  GLint diffuseTexLoc{glGetUniformLocation(program, "diffuseTex")};
+  GLint mappingModeLoc{glGetUniformLocation(program, "mappingMode")};
+
+  // Set uniform variables used by every scene object
+  glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, &m_viewMatrix[0][0]);
+  glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, &m_projMatrix[0][0]);
+  
+  //camera
+  // Set uniform variables for viewMatrix and projMatrix
+  // These matrices are used for every scene object
+  glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, &m_camera.m_viewMatrix[0][0]);
+  glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, &m_camera.m_projMatrix[0][0]);
+
+
+  glUniform1i(diffuseTexLoc, 0);
+  glUniform1i(mappingModeLoc, m_mappingMode);
+
+  auto lightDirRotated{m_trackBallLight.getRotation() * m_lightDir};
+  glUniform4fv(lightDirLoc, 1, &lightDirRotated.x);
+  glUniform4fv(IaLoc, 1, &m_Ia.x);
+  glUniform4fv(IdLoc, 1, &m_Id.x);
+  glUniform4fv(IsLoc, 1, &m_Is.x);
+
+
+
+
+  // Set uniform variables of the current object
+  glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix[0][0]);
+
+  auto modelViewMatrix{glm::mat3(m_viewMatrix * m_modelMatrix)};
+  glm::mat3 normalMatrix{glm::inverseTranspose(modelViewMatrix)};
+  glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+  glUniform1f(shininessLoc, m_shininess);
+  glUniform4fv(KaLoc, 1, &m_Ka.x);
+  glUniform4fv(KdLoc, 1, &m_Kd.x);
+  glUniform4fv(KsLoc, 1, &m_Ks.x);
+
+  m_model.render(m_trianglesToDraw);
+
+  glUseProgram(0);
+}
+
+void OpenGLWindow::paintUI() {
+  abcg::OpenGLWindow::paintUI();
+
+
+  // Only in WebGL
+  #if defined(__EMSCRIPTEN__)
+    fileDialogModel.SetPwd(getAssetsPath());
+    fileDialogTex.SetPwd(getAssetsPath() + "/maps");
+  #endif
+   // Slider will be stretched horizontally
+  m_trianglesToDraw = m_model.getNumTriangles();
+
+  glEnable(GL_CULL_FACE);
+
+  // CW/CCW combo box
+  glFrontFace(GL_CCW);
+
+  // Projection combo box
+  auto aspect{static_cast<float>(m_viewportWidth) /
+                static_cast<float>(m_viewportHeight)};
+  m_projMatrix = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 5.0f);
+  // UV mapping box
+
+  m_mappingMode = 3;
+}
+
+
+void OpenGLWindow::resizeGL(int width, int height) {
+  m_viewportWidth = width;
+  m_viewportHeight = height;
+
+  m_trackBallModel.resizeViewport(width, height);
+  m_trackBallLight.resizeViewport(width, height);
+
+  m_camera.computeProjectionMatrix(width, height);
+
+}
+
+void OpenGLWindow::terminateGL() {
+  for (const auto& program : m_programs) {
+    glDeleteProgram(program);
+  }
+}
+
+void OpenGLWindow::update() {
+  m_modelMatrix = m_trackBallModel.getRotation();
+
+  //UPDATE
+  //m_model.update(m_trackBallModel);
+  float deltaTime{static_cast<float>(getDeltaTime())};
+
+  m_viewMatrix =
+      glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f + m_zoom),
+                  glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    // Update LookAt camera
+  m_camera.dolly(m_dollySpeed * deltaTime);
+  m_camera.truck(m_truckSpeed * deltaTime);
+  m_camera.pan(m_panSpeed * deltaTime);
+
+}
